5ad1b6aa195df65f8476b88dc3b3fe931998406e
Jiehfeng <alainjiehfeng@gmail.com>
2023-01-03 00:29:24 +0530

    ok

--- android/src/main/kotlin/com/kino/video_360/player/VRActivity.kt
+++ android/src/main/kotlin/com/kino/video_360/player/VRActivity.kt
@@ -49,7 +49,7 @@ class VRActivity : Activity(), Player.Listener {
         (vrPlayer.videoSurfaceView as SphericalGLSurfaceView)
                 .setDefaultStereoMode(C.STEREO_MODE_STEREO_MESH)
 
-        vr_player.setUseSensorRotation(false)
+        vrplayer.setUseSensorRotation(false)
 
         bandwidthMeter = DefaultBandwidthMeter.Builder(this)
                 .build()
